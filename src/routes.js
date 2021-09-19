c7a0f6ad685beeead2f359de1894737ed8a3bd79
Igor Fastroni Correa <igor.correa@linx.com.br>
2021-09-19 11:22:08 -0300

    versao final

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const router = express.Router();
 
-/*const TrainingController = require('./app/controllers/training/TrainingController');
+const TrainingController = require('./app/controllers/training/TrainingController');
 const FonoController = require('./app/controllers/fono/FonoController')
 const AuthFonoController = require('./app/controllers/fono/AuthFonoController')
 const AuthPatientController = require('./app/controllers/patient/AuthPatientController')
@@ -11,12 +11,12 @@ const PatientController = require('./app/controllers/patient/PatientController')
 const TrainingListController = require('./app/controllers/patient/TrainingListController')
 const TrainingDetailController = require('./app/controllers/patient/TrainingDetailController')
 const Monitoring = require('./app/controllers/fono/Monitoring')
-*/
+
 router.get('/version', (req, res) => res.status(200).send({ version: "0.0.1" }));
 
 
 //WEB
-/*router.post('/fono', FonoController.post);
+router.post('/fono', FonoController.post);
 router.post('/auth/fono', AuthFonoController.post);
 router.post('/training', TrainingController.post);
 router.get('/training', TrainingController.get);
@@ -30,6 +30,6 @@ router.put('/patient/training/:trainingId', TrainingDetailController.put);
 
 
 router.post('/monitoring', Monitoring.post)
-router.get('/monitoring', TrainingController.get)*/
+router.get('/monitoring', TrainingController.get)
 
 module.exports = router;
