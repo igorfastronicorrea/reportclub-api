0d74714bb383ae01e75b2b88b7408811a62b82c7
Igor Fastroni Correa <igor.correa@linx.com.br>
2021-09-19 18:43:45 -0300

    teste

--- src/app/database/index.js
+++ src/app/database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.set('useCreateIndex', true);
-mongoose.connect('mongodb://igor:1234@147.182.176.63:27017/easytraining', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://127.0.0.1:27017/easytraining', { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose
