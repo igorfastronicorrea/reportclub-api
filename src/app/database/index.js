b24c90d57e90123c8aff5a73e62ea17b143f02ce
Igor Fastroni Correa <igor.correa@linx.com.br>
2021-09-20 18:35:56 -0300

    atlas mongo

--- src/app/database/index.js
+++ src/app/database/index.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.set('useCreateIndex', true);
-mongoose.connect('mongodb://localhost/easytraining', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://dbuser:db1234@cluster0.2sros.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('mongodb://localhost/easytraining', { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose
